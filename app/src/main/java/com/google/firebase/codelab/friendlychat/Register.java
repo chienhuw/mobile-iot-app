73198cb8e715d3441d5d971ca03d9cbc6dedaed0
chienhuw <chienhuw@andrew.cmu.edu>
2018-05-04 01:12:07 -0400

    register update

--- app/src/main/java/com/google/firebase/codelab/friendlychat/Register.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/Register.java
@@ -1,6 +1,9 @@
 package com.google.firebase.codelab.friendlychat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -16,6 +19,7 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,6 +33,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Register extends AppCompatActivity{
 
@@ -57,6 +64,25 @@ public class Register extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            mFirebaseUser.getUid();
+           // if (mFirebaseUser.getPhotoUrl() != null) {
+           //     mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+           // }
+
+        }
+
+
 
         // address name & address
         mName = (TextView) findViewById(R.id.nametextView);
@@ -98,6 +124,8 @@ public class Register extends AppCompatActivity{
             }
         });
 
+
+
     }
 
     @Override
@@ -106,11 +134,14 @@ public class Register extends AppCompatActivity{
         if (resultCode == Activity.RESULT_OK) {
             final Place place = PlacePicker.getPlace(this, data);
             final CharSequence name = place.getName();
+
+
             final CharSequence address = place.getAddress();
             String attributions = (String) place.getAttributions();
             if (attributions == null) {
                 attributions = "";
             }
+
 
             switch(requestCode) {
                 case 0:
@@ -143,23 +174,68 @@ public class Register extends AppCompatActivity{
         phoneText = (EditText) findViewById(R.id.editText3);
         mAddress = (TextView) findViewById(R.id.addtextView);
         mAddress2 = (TextView) findViewById(R.id.addtextView2);
+
         String name = nameText.getText().toString();
         String phone = phoneText.getText().toString();
         String pick = mAddress.getText().toString();
         String drop = mAddress2.getText().toString();
+        String dropName = mName2.getText().toString();
+
+        LatLng pickGeo = getLocationFromAddress(this,pick);
+        String picklat = String.valueOf(pickGeo.latitude);
+        String picklong = String.valueOf(pickGeo.longitude);
+
+        LatLng dropGeo = getLocationFromAddress(this,drop);
+        String droplat = String.valueOf(dropGeo.latitude);
+        String droplong = String.valueOf(dropGeo.longitude);
+
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        User user1 = new User(name, name, "http://somephoto.jpg","52.567","34.525", pick,"I am cooler", drop,"234123123");
-
-
+        User user1 = new User(mFirebaseUser.getUid(), name, "http://somephoto.jpg",picklat,picklong, pick,"I am cooler", drop,phone);
         mDatabase.child(mUsersChild).child(user1.userid).setValue(user1);
-       // mDatabase.child(maddreessChild1).child(user1.userid).setValue(drop);
 
+        //dropoff table
+        //mDatabase.child(maddreessChild1).setValue(dropName);
+        mDatabase.child(maddreessChild1).child(dropName).child("latitude").setValue(droplat);
+        mDatabase.child(maddreessChild1).child(dropName).child("longitude").setValue(droplong);
+        mDatabase.child(maddreessChild1).child(dropName).child("users").child(mFirebaseUser.getUid()).setValue(mFirebaseUser.getUid());
 
+        //mDatabase.child(maddreessChild1).child(dropName).setValue();
 
 
         Intent intent = new Intent(this, MainPage.class);
         startActivity(intent);
+    }
+
+    public LatLng getLocationFromAddress(Context context, String inputtedAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng resLatLng = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(inputtedAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            if (address.size() == 0) {
+                return null;
+            }
+
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            resLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        return resLatLng;
     }
 
 }
